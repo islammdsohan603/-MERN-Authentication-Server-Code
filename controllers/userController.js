fcbbb7b89b8465848fa7ded9c672978f3fdf3b7c
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-28 21:41:41 +0600

    verify token

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,3 +59,61 @@ export const registerUser = async (req, res) => {
     })
   }
 }
+
+
+export const verification = async (req, res) => {
+  try {
+    const authHeader = req.haaders.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization token is missing or invalid"
+      })
+    }
+
+    const token = authHeader.split(" ")[1]
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET_KEY)
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(400).json({
+          success: false,
+          message: "The registration token has expired"
+        })
+      }
+
+      return res.status(400).json({
+        success: false,
+        message: "Token verification failed"
+      })
+
+    }
+
+    const user = await User.findById(decoded.id)
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      })
+
+
+
+    }
+
+    user.token = null
+    user.isVerified = true
+    await user.save()
+
+    return res.status(200).json({
+      success: true,
+      message: "Email Verified successfully"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
