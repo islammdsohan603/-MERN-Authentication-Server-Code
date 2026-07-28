dbf280a886d330bca49bf0ab29b93b76c7addda0
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-28 17:33:34 +0600

    register error fix and code upto clean

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_DB_URI}/note-app`)
+    await mongoose.connect(`${process.env.MONGO_DB_URI}`)
     console.log("MongoDb Connected Successfully")
   } catch (error) {
     console.log("MongoDb connection error ", error)
--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-connectDB();
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 
 app.use("/user", userRoute);
 
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is listening at PORT ${PORT}`);
-});
+}); 
