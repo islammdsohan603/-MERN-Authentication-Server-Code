d82c6ecf2996dbd4dee63ef3d2030d5bd26eda33
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-29 16:08:49 +0600

    user logout functian added

--- controllers/userController.js
+++ controllers/userController.js
@@ -190,3 +190,22 @@ export const loginUser = async (req, res) => {
     })
   }
 }
+
+export const logoutUser = async (req, res) => {
+  try {
+    const userId = req.userId;
+    await Session.deleteMany({ userId });
+    await User.findByIdAndUpdate(userId, { isLoggedIn: false })
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { loginUser, registerUser, verification } from "../controllers/userController.js";
+import { loginUser, logoutUser, registerUser, verification } from "../controllers/userController.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/verify", verification);
 router.post("/login", loginUser);
+router.post("/logout", isAuthenticated, logoutUser);
 
 export default router;
