2c1e5818652b97bdfaf695d624c6f9d2437956e7
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-30 06:56:31 +0600

    userValidate schema added

---
+++ validators/userValidate.js
@@ -0,0 +1,31 @@
+
+
+import yup, { Schema } from 'yup'
+
+export const userSchema = yup.object({
+  username: yup
+    .string()
+    .trim()
+    .min(3, "Username must be atleast of 3 charactors")
+    .required(),
+  email: yup
+    .string()
+    .email("The email is not valid one")
+    .required(),
+  password: yup
+    .string()
+    .min(4, "Password must be atleast 4 character")
+    .required()
+})
+
+export const validateUser = (schema) => async (req, res) => {
+  try {
+    await userSchema.validate(req.body)
+    next()
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
